66ee263ee4df48e2662047e9d605caf2f019771f
Gustavo-Caceres <gustavocaceres914@gmail.com>
2026-09-14 09:33:01 -0300

    chore: update environment variable references and add .env.example file

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -9,10 +9,10 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') })
 
 const mongoUser = process.env.MONGO_USER;
-const mongoPassword = process.env.db_password;
-const mongoHost = 'localhost';
-const mongoPort = 27017;
-const mongoDatabase = 'order-management';
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoDatabase = process.env.MONGO_DATABASE || 'order-management';
 
 const connectionString = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}?authSource=admin`;
 
